bf1ebe7355d1dcb29be23e07d54ea9bce6e09215
salekelfahim <salekelfahim@gmail.com>
2024-03-27 10:49:45 +0000

    Insstalling project

---
+++ database/migrations/2024_03_27_103349_create_wallets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->decimal('balance')->default(0.00);
+            $table->unsignedBigInteger('user_id')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
